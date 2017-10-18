ce4b16bc93f6d12745f712aa0512b83e3dd5435e
Benja <benja.mora.torres@gmail.com>
2017-10-18 13:37:10 -0300

    Nuevas

---
+++ Biblioteca/ClasesExterior/Pais.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca.ClasesExterior
+{
+    public class Pais
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public G() {
+            Init();
+        }
+
+        private void Init() {
+            id = 0;
+            nombre = String.Empty;
+        }
+    }
+}

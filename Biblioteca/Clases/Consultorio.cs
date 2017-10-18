7f87e2478e1f549aa0fca931c84533dc037ab830
Benja <benja.mora.torres@gmail.com>
2017-10-18 16:24:09 -0300

    mas clases

---
+++ Biblioteca/Clases/Consultorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Biblioteca.ClasesExterior;
+
+namespace Biblioteca.Clases
+{
+    public class Consultorio
+    {
+        private int id;
+        private int rut;
+        private char dv;
+        private Comuna comuna;
+        private String nombre;
+
+    }
+}

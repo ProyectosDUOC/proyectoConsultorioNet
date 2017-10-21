5cca81636ffc3ac4432bef5a770682e10a8f40fc
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2017-10-21 00:57:21 -0400

    alergia CRUD

--- Biblioteca/ClasesExterior/Alergia.cs
+++ Biblioteca/ClasesExterior/Alergia.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Biblioteca.Clases;
+using Biblioteca;
 
 namespace Biblioteca.ClasesExterior
 {
@@ -30,5 +32,93 @@ namespace Biblioteca.ClasesExterior
             id = 0;
             nombre = String.Empty;
         }
+
+        public bool Create() 
+        {
+            try 
+            {
+                Consultiorios.DALC.Alergia alergia = new Consultiorios.DALC.Alergia();
+
+                alergia.id_alergia = this.Id;
+                alergia.nombre = this.nombre;
+
+                CommonBC.ModeloConsultorio.AddToAlergia(alergia);
+                CommonBC.ModeloConsultorio.SaveChanges();
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Read()
+        {
+            try
+            {
+                Consultiorios.DALC.Alergia alergia =
+                    CommonBC.ModeloConsultorio.Alergia.First
+                        (
+                            ale => ale.id_alergia == this.Id
+                        );
+
+
+                this.Id = alergia.id_alergia;
+                this.Nombre = alergia.nombre;
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Update()
+        {
+            try
+            {
+                Consultiorios.DALC.Alergia alergia =
+                    CommonBC.ModeloConsultorio.Alergia.First
+                        (
+                            ale => ale.id_alergia == this.Id
+                        );
+
+
+                this.Id = alergia.id_alergia;
+                this.Nombre = alergia.nombre;
+
+                CommonBC.ModeloConsultorio.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Update()
+        {
+            try
+            {
+                Consultiorios.DALC.Alergia alergia =
+                    CommonBC.ModeloConsultorio.Alergia.First
+                        (
+                            ale => ale.id_alergia == this.Id
+                        );
+
+                CommonBC.ModeloConsultorio.DeleteObject(alergia);
+                CommonBC.ModeloConsultorio.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        
+
+
     }
 }

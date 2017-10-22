ad61214bcd2a933189dee127fc0d31b2cf35d1f1
Benja <benja.mora.torres@gmail.com>
2017-10-21 21:40:06 -0300

    Consultorio Enfermera Medico CRUD

--- Biblioteca/Clases/Enfermera.cs
+++ Biblioteca/Clases/Enfermera.cs
@@ -40,5 +40,82 @@ namespace Biblioteca.Clases
             jornadaLaboral = null;
         }
 
+        public bool Create() {
+            try
+            {
+                Consultiorios.DALC.Enfermera enfermera = new Consultiorios.DALC.Enfermera();
+
+                enfermera.id_enfermera = this.Id;
+                enfermera.id_usuario = this.Usuario.Id;
+                enfermera.id_jornada_laboral = this.JornadaLaboral.Id;
+
+                CommonBC.ModeloConsultorio.AddToEnfermera(enfermera);
+                CommonBC.ModeloConsultorio.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {                
+                return false;
+            }
+        }
+
+        public bool Read() {
+            try
+            {
+                Consultiorios.DALC.Enfermera enfermera = CommonBC.ModeloConsultorio.Enfermera.First
+                    (
+                        enfer => enfer.id_enfermera == this.Id
+                    );
+                this.Id = enfermera.id_enfermera;
+                this.Usuario.Id = enfermera.id_usuario;
+                this.JornadaLaboral.Id = enfermera.id_jornada_laboral;
+
+                CommonBC.ModeloConsultorio.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {                
+               return false;
+            }        
+        }
+
+        public bool Update() {
+            try
+            {
+                Consultiorios.DALC.Enfermera enfermera = CommonBC.ModeloConsultorio.Enfermera.First
+                    (
+                        enfer => enfer.id_enfermera == this.Id
+                     );
+                enfermera.id_enfermera = this.Id;
+                enfermera.id_usuario = this.Usuario.Id;
+                enfermera.id_jornada_laboral = this.JornadaLaboral.Id;
+
+                CommonBC.ModeloConsultorio.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {                
+                return false;
+            }        
+        }
+        public bool Delete() {
+            try
+            {
+                Consultiorios.DALC.Enfermera enfermera = CommonBC.ModeloConsultorio.Enfermera.First
+                    (
+                        enfer => enfer.id_enfermera == this.Id
+                    );
+                CommonBC.ModeloConsultorio.DeleteObject(enfermera);
+                CommonBC.ModeloConsultorio.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }        
+        }
+        
+        
+
     }
 }

9936e9f03d4096d9eb78cea452ce6d24d45eee88
Benja <benja.mora.torres@gmail.com>
2017-10-22 03:23:52 -0300

    Coleccion ControlAcceso, fichaPaciente

--- Biblioteca/Control/ControlAcceso.cs
+++ Biblioteca/Control/ControlAcceso.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Biblioteca.ClasesExterior;
+using Biblioteca.Clases;
 
 namespace Biblioteca.Clases
 {
---
+++ Biblioteca/Control/FichaPacienteColeccion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca.Control
+{
+    public class FichaPacienteColeccion
+    {
+        private List<FichaPaciente> GenerarListado(List<Consultiorios.DALC.Ficha_Paciente> fichaPacienteDALC) 
+        {
+            List<Biblioteca.Control.FichaPaciente> fichasPacientes = new List<FichaPaciente>();
+            foreach (Consultiorios.DALC.Ficha_Paciente fp in fichaPacienteDALC)
+            {
+                Biblioteca.Control.FichaPaciente ficha = new FichaPaciente();
+                ficha.Id_ficha_paciente = fp.id_ficha_paciente;
+                ficha.Fecha = (DateTime)fp.fecha;
+                ficha.Consultorio.Id = fp.Consultorio.id_consultorio;
+                ficha.Secretaria.Id = fp.Secretaria.id_secretaria;
+                ficha.Medico.Id = fp.Medico.id_Medico;
+                ficha.Enfermera.Id = fp.Enfermera.id_enfermera;
+                ficha.Paciente.Id = fp.Paciente.id_paciente;
+                ficha.Peso = fp.peso;
+                ficha.Estatura = fp.estatura;
+                ficha.Imc = fp.imc;
+                ficha.Temperatura = fp.temperatura;
+                ficha.Sistonica = fp.sistonica;
+                ficha.Distolica = fp.distolica;
+                ficha.Pulsacion = fp.pulsacion;
+                ficha.Diastolica = fp.diastolica;
+
+                fichasPacientes.Add(ficha);
+            }
+
+            return fichasPacientes;
+        }
+
+        public List<FichaPaciente> ReadAll() {
+            var fichaPaciente = CommonBC.ModeloConsultorio.Ficha_Paciente;
+            return GenerarListado(fichaPaciente.ToList());
+        }
+    }
+}

a741b1ace514d59848cb8fe68d234ceba2cb7177
Benja <benja.mora.torres@gmail.com>
2017-10-22 03:44:09 -0300

    Licencia Medica Coleccion

---
+++ Biblioteca/Control/LicenciaMedColeccion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Consultiorios.DALC;
+using Biblioteca.Clases;
+
+namespace Biblioteca.Control
+{
+    public class LicenciaMedColeccion
+    {
+        private List<LicenciaMedica> GenerarListado(List<Consultiorios.DALC.Licencia_Medica> licenciaMedicaDALC) 
+        {
+            List<Biblioteca.Control.LicenciaMedica> licenciasMedicas = new List<LicenciaMedica>();
+            foreach (Consultiorios.DALC.Licencia_Medica lm in licenciaMedicaDALC)
+            {
+                Biblioteca.Control.LicenciaMedica licenciasMe = new LicenciaMedica();
+
+                licenciasMe.IdLicencia = lm.id_licencia_medica;
+                licenciasMe.Motivo = lm.motivos;
+                licenciasMe.NumeroDias = lm.numero_de_dias;
+                licenciasMe.Paciente.Id = lm.id_ficha_paciente;
+
+                licenciasMedicas.Add(licenciasMe);
+            }
+
+
+            return licenciasMedicas;
+        }
+
+        public List<LicenciaMedica> ReadAll()
+        {
+            var licenciaM = CommonBC.ModeloConsultorio.Licencia_Medica;
+            return GenerarListado(licenciaM.ToList());
+        }
+
+    }
+}

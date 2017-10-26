08f5096027a7a1a2c980e92a95666bf7271c0534
Paty <paathyfernandacg03@gmail.com>
2017-10-25 13:17:21 -1200

    Coleccion de clases

--- Biblioteca/Clases/SecretariaColeccion.cs
+++ Biblioteca/Clases/SecretariaColeccion.cs
@@ -5,7 +5,32 @@ using System.Text;
 
 namespace Biblioteca.Clases
 {
-    class SecretariaColeccion
+    public class SecretariaColeccion
     {
+        public List<Secretaria> GenerarListado(List<Consultiorios.DALC.Secretaria> SecretariaDALC)
+        {
+            List<Biblioteca.Clases.Secretaria> secretarias= new List<Secretaria>();
+
+            foreach (Consultiorios.DALC.Secretaria secre in SecretariaDALC)
+            {
+                Biblioteca.Clases.Secretaria secretaria= new Secretaria();
+
+                secretaria.Id = secre.id_secretaria;
+                secretaria.Usuario.Id = secre.id_usuario;
+                secretaria.JornadaLaboral.Id = secre.id_jornada_laboral;
+                
+                secretarias.Add(secretaria);
+            }
+            return secretarias;
+        }
+
+        //Listado de todos los elementos
+
+        public List<Secretaria> ReadAll()
+        {
+            var secretarias = CommonBC.ModeloConsultorio.Secretaria;
+            return GenerarListado(secretarias.ToList());
+        }
+    
     }
 }

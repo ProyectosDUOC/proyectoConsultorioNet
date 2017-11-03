5f0c0dfc9a9565f2b98d68c3412d97bdd2a361ae
carlos <carlosorellana.oa@gmail.com>
2017-11-03 02:33:58 -0300

    2:12 03/11/2017

--- Biblioteca/ClasesExterior/GrupoSanguineoColeccion.cs
+++ Biblioteca/ClasesExterior/GrupoSanguineoColeccion.cs
@@ -7,11 +7,20 @@ namespace Biblioteca.ClasesExterior
 {
     public class GrupoSanguineoColeccion
     {
-        public List<GrupoSanguineo> GenerarListado(List<Consultiorios.DALC.Grupo_sanguineo> GrupoSanguineoDALC)
+        public List<GrupoSanguineo> GenerarListado (List<Consultiorios.DALC.Grupo_sanguineo> GrupoSanguienoDAlC) 
         {
-            List<Biblioteca.ClasesExterior.GrupoSanguineo> gruposS = new List<GrupoSanguineo>();
+            List<Biblioteca.ClasesExterior.GrupoSanguineo> gruposSanguineos = new List<GrupoSanguineo>();
 
+            foreach (Consultiorios.DALC.Grupo_sanguineo grupS in GrupoSanguienoDAlC)
+            {
+                Biblioteca.ClasesExterior.GrupoSanguineo gruposSanguineo = new GrupoSanguineo();
 
+                gruposSanguineo.Id = grupS.id_grupo_sanguineo;
+                gruposSanguineo.Nombre = grupS.nombre;
+
+                gruposSanguineos.Add(gruposSanguineo);
+            }
+            return gruposSanguineos;
         }
     }
 

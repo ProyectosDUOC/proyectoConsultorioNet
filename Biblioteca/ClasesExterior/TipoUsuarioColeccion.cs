9f9e43cbeb208d340d26a898155dda81feb52a66
Seba Orrego <sebastian_orrego_a@yahoo.es>
2017-11-05 23:01:54 -0300

    tipousuario coleccion terminado

--- Biblioteca/ClasesExterior/TipoUsuarioColeccion.cs
+++ Biblioteca/ClasesExterior/TipoUsuarioColeccion.cs
@@ -5,7 +5,21 @@ using System.Text;
 
 namespace Biblioteca.ClasesExterior
 {
-    class TipoUsuarioColeccion
+    public class TipoUsuarioColeccion
     {
+        List<TipoUsuario> GenerarListado(List<Consultiorios.DALC.Tipo_Usuario> usuariosDALC)
+        {
+            List<TipoUsuario> tipo_usuarios = new List<TipoUsuario>();
+
+            foreach (Consultiorios.DALC.Tipo_Usuario tipouserDALC in usuariosDALC)
+            {
+                TipoUsuario tipouser = new TipoUsuario();
+                tipouser.Id = tipouserDALC.id_tipo_usuario;
+                tipouser.Nombre = tipouserDALC.nom_tipo_usuario;
+
+                tipo_usuarios.Add(tipouser);
+            }
+            return tipo_usuarios;
+        }
     }
 }
